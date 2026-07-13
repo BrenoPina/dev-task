abe9b29bd651ab0e4154f9fd7caf6aad96b6e1c3
Breno Pina <breno.rodrigues08@hotmail.com>
2026-07-13 15:27:59 -0300

    feat: create task form modal with toggle behavior

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,27 @@
 import { tasks } from '../../data.js';
 
 const board = document.querySelector('.board');
+const btnOpenModal = document.querySelector('#btn-open-modal');
+const btnCancel = document.querySelector('#btn-cancel');
+const btnClose = document.querySelector('#btn-close');
+const modal = document.querySelector('#modal');
+
+function closeModal() {
+  modal.close();
+}
+
+function openModal() {
+  modal.showModal();
+}
+
+btnOpenModal.addEventListener('click', openModal);
+btnCancel.addEventListener('click', closeModal);
+btnClose.addEventListener('click', closeModal);
+modal.addEventListener('click', event => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
 
 const lists = {
   todo: document.querySelector('.column__list--todo'),
